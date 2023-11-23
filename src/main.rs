6d6891ba400a7aa02413d95a79c14c9dc927c697
noah <noahshin11@gmail.com>
2023-11-23 13:32:54 +0900

    let mut

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::io;
 
 fn main() {
-    println!("[가위, 바위, 보] 중 하나를 입력하세요!");
+    // println!("[가위, 바위, 보] 중 하나를 입력하세요!");
 
-    let mut decision = String::new();
+    // let mut decision = String::new();
 
-    io::stdin().read_line(&mut decision)
-        .expect("입력 실패");
+    // io::stdin().read_line(&mut decision)
+    //     .expect("입력 실패");
 
-    println!("당신의 선택은.... 바로 = {decision}");
+    // println!("당신의 선택은.... 바로 = {decision}");
+
+    let mut x = 3;
+    println!("x의 값은 {x}입니다,, ㅎㅎ");
+
+    x = 7;
+
+    println!("x의 값은 {x}입니다,, ㅎㅎ");
 }
